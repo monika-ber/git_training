347cadbf326204c6b29b84154d506486dde7b1a5
monika-ber <monka581@gmail.com>
2024-09-23 14:46:38 +0200

    Added another exclamation mark in Main.java

--- src/main/java/com/capgemini/mackowal/Main.java
+++ src/main/java/com/capgemini/mackowal/Main.java
@@ -3,7 +3,7 @@ import io.javalin.Javalin;
 public class Main {
     public static void main(String[] args){
         Javalin app = Javalin.create().start(8000);
-        app.get("/", ctx->ctx.result("Hello world!"));
+        app.get("/", ctx->ctx.result("Hello world!!"));
         app.get("/user1", ctx->ctx.result("Adam Nowak"));
         app.get("/user2",ctx->ctx.result("Jan Nowak"));
 	  app.get("/user3",ctx->ctx.result("Jan Kowalski"));
